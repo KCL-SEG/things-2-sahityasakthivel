c5fa772f87d6b4694ec84d38866c71b822ae8341
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-10 11:32:30 +0100

    Completed Task by creating form

--- things/forms.py
+++ things/forms.py
@@ -3,6 +3,6 @@ from django import forms
 # Create your forms here.
 
 class ThingForm(forms.Form):
-    name = forms.CharField(label = 'Name')
+    name = forms.CharField(label = 'Name', max_length = 35)
     description = forms.CharField(widget = forms.Textarea)
-    quantity = forms.CharField(widget = forms.NumberInput)
+    quantity = forms.CharField(widget = forms.NumberInput(attrs={'min':0, 'max' : 50}))
