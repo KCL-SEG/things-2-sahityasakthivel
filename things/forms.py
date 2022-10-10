6652d1c3625e72f2b176a9558900513b427bd24f
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-10 11:49:00 +0100

    Add restrictions for description

--- things/forms.py
+++ things/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class ThingForm(forms.Form):
     name = forms.CharField(label = 'Name', max_length = 35)
-    description = forms.CharField(maxlength= 120, empty_value = False,widget = forms.Textarea)
+    description = forms.CharField(max_length= 120, empty_value = False,widget = forms.Textarea)
     quantity = forms.IntegerField(min_value = 0, max_value = 50,widget = forms.NumberInput)
