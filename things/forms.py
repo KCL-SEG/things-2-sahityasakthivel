910fabdb2daef69bbcd03a4fc72379016cdd5d2f
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-10 11:28:53 +0100

    Completed Task by creating form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,8 @@
 """Forms of the project."""
-
+from django import forms
 # Create your forms here.
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label = 'Name')
+    description = forms.CharField(widget = forms.Textarea)
+    quantity = forms.CharField(widget = forms.NumberInput)
